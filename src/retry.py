634959884a4a77adab844e49d031df0e7534f47b
ALuong81 <anhln81@gmail.com>
2026-04-24 03:30:03 +0700

    Update retry.py

--- src/retry.py
+++ src/retry.py
@@ -1,9 +1,17 @@
 import time
 
-def retry(func, n=3):
-    for _ in range(n):
+def retry(func, max_retry=3):
+
+    for i in range(max_retry):
         try:
             return func()
-        except:
-            time.sleep(1)
+        except Exception as e:
+
+            if "Rate Limit" in str(e):
+                print("⏳ WAIT RATE LIMIT...")
+                time.sleep(10)
+
+            else:
+                time.sleep(2)
+
     return None
