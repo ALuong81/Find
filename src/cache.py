4e4ee30ebdf5c66135bc16d73e9e87e7a5bbf77e
ALuong81 <anhln81@gmail.com>
2026-04-23 12:12:25 +0700

    Update cache.py

--- src/cache.py
+++ src/cache.py
@@ -1,16 +1,18 @@
 import os
 import pandas as pd
+import time
 
 CACHE_DIR = "data/cache"
-os.makedirs(CACHE_DIR, exist_ok=True)
-
-def path(symbol):
-    return f"{CACHE_DIR}/{symbol}.csv"
 
 def load_cache(symbol):
-    if os.path.exists(path(symbol)):
-        return pd.read_csv(path(symbol))
-    return None
 
-def save_cache(symbol, df):
-    df.to_csv(path(symbol), index=False)
+    path = f"{CACHE_DIR}/{symbol}.csv"
+
+    if not os.path.exists(path):
+        return None
+
+    # 🔥 nếu cache < 1 ngày → dùng luôn
+    if time.time() - os.path.getmtime(path) < 86400:
+        return pd.read_csv(path)
+
+    return None
