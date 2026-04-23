8a0f93952cb71f338e8d24925add0fbf9ddfd21c
ALuong81 <anhln81@gmail.com>
2026-04-24 04:28:54 +0700

    Update market_ranking.py

--- src/market_ranking.py
+++ src/market_ranking.py
@@ -1,6 +1,4 @@
 from data_loader import load_index
-from relative_strength import relative_strength
-
 
 def market_ranking(df_symbols, load_stock_data):
 
@@ -9,22 +7,19 @@ def market_ranking(df_symbols, load_stock_data):
     results = []
 
     for _, row in df_symbols.iterrows():
-
         try:
             df = load_stock_data(row["symbol"])
 
             ret = df["close"].pct_change(20).iloc[-1]
             vol = df["volume"].iloc[-1] / df["volume"].rolling(20).mean().iloc[-1]
-            rs = relative_strength(df, df_index)
 
-            score = ret*0.4 + rs*0.4 + vol*0.2
+            score = ret*0.6 + vol*0.4
 
             results.append({
                 "symbol": row["symbol"],
                 "sector": row["sector"],
                 "score": score
             })
-
         except:
             continue
 
