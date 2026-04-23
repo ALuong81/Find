99322b18432f8cd132666c7af5c6e247c0b86c98
ALuong81 <anhln81@gmail.com>
2026-04-24 04:24:30 +0700

    Update preload_data.py

--- src/preload_data.py
+++ src/preload_data.py
@@ -1,34 +1,24 @@
 import pandas as pd
+import os
 from data_loader import load_stock_data
 from symbol_loader import load_symbols
-import os
 
 SAVE_DIR = "data/market"
 os.makedirs(SAVE_DIR, exist_ok=True)
-
 
 def main():
 
     df_symbols = load_symbols()
 
-    print("🚀 PRELOAD DATA:", len(df_symbols))
-
     for _, row in df_symbols.iterrows():
-
         symbol = row["symbol"]
 
         try:
             df = load_stock_data(symbol)
-
             df.to_csv(f"{SAVE_DIR}/{symbol}.csv", index=False)
-
-            print("✅", symbol)
-
-        except Exception as e:
-            print("❌", symbol)
-
-    print("DONE PRELOAD")
-
+            print("OK", symbol)
+        except:
+            print("FAIL", symbol)
 
 if __name__ == "__main__":
     main()
