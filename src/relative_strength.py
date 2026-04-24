b95fadd7386e9eaaad3ee870ae7b0bbd66a017fe
ALuong81 <anhln81@gmail.com>
2026-04-24 08:15:38 +0700

    Update relative_strength.py

--- src/relative_strength.py
+++ src/relative_strength.py
@@ -1,6 +1,14 @@
-def relative_strength(df, df_index):
+def relative_strength(df_stock, df_index):
 
-    r1 = df["close"].pct_change(20).iloc[-1]
-    r2 = df_index["close"].pct_change(20).iloc[-1]
+    if df_stock is None or df_index is None:
+        return 0
 
-    return r1 - r2
+    if len(df_stock) < 20 or len(df_index) < 20:
+        return 0
+
+    stock_return = df_stock["close"].pct_change(20).iloc[-1]
+    index_return = df_index["close"].pct_change(20).iloc[-1]
+
+    rs = stock_return - index_return
+
+    return rs
