5a41b78eda0eb34834985215ea217e1b6ff88400
ALuong81 <anhln81@gmail.com>
2026-04-24 12:39:29 +0700

    Implement error handling in relative_strength function
    Added error handling to the relative_strength function.

--- src/relative_strength.py
+++ src/relative_strength.py
@@ -1,14 +1,19 @@
 def relative_strength(df_stock, df_index):
 
-    if df_stock is None or df_index is None:
+    try:
+        if df_stock is None or df_index is None:
+            return 0
+
+        if len(df_stock) < 30 or len(df_index) < 30:
+            return 0
+
+        stock_return = df_stock["close"].pct_change(20).iloc[-1]
+        index_return = df_index["close"].pct_change(20).iloc[-1]
+
+        rs = stock_return - index_return
+
+        return rs
+
+    except Exception as e:
+        print("RS ERROR:", e)
         return 0
-
-    if len(df_stock) < 20 or len(df_index) < 20:
-        return 0
-
-    stock_return = df_stock["close"].pct_change(20).iloc[-1]
-    index_return = df_index["close"].pct_change(20).iloc[-1]
-
-    rs = stock_return - index_return
-
-    return rs
