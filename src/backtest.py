c5d280c9c5aa27ecfaee203faf50f0bf154dc6c5
ALuong81 <anhln81@gmail.com>
2026-04-30 13:29:34 +0700

    Include symbol in entry score print statement

--- src/backtest.py
+++ src/backtest.py
@@ -254,7 +254,7 @@ def run_backtest(start_date="2023-01-01"):
             df = df_full[df_full["date"] <= date]
 
             f = entry_score(df)
-            print(f)
+            print(symbol, f)
             
             if f is None:
                 continue
